1f4cfebb4da8b3df07507ff5949ebfc77fe1e000
Pravin Bhagade <pbhagade@cloudera.com>
2022-04-07 11:39:17 +0530

    Create k8-setup.sh

---
+++ k8-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yum check-update
+yum install -y yum-utils device-mapper-persistent-data lvm2 wget git vim
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
+yum install docker-ce -y
+systemctl start docker && systemctl enable docker && systemctl status docker
+setenforce 0 && sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sed -i '/swap/d' /etc/fstab && swapoff -a
+
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm && systemctl enable kubelet.service
+
+echo "kubeadm init"
+# kubeadm reset
+# kubeadm token create --print-join-command
+
+# kubelet troubleshoting # journalctl -b -f -u kubelet.service
