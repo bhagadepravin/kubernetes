96fb138efd6fa35df0580d6ceb8662d02bf3cc9d
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-06-25 16:37:56 +0530

    Update k8-setup.sh

--- k8-setup.sh
+++ k8-setup.sh
@@ -130,7 +130,8 @@ function install_k8 {
         kubectl get nodes
 
         logStep "Remove "node-role.kubernetes.io/master:NoSchedule taint", if its a single node cluster and you want deploy pods on control plane as well..\n"
-        logSuccess "kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-\n"
+        logError "kubectl taint nodes $(hostname) node-role.kubernetes.io/master:NoSchedule-\n"
+        logError "kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-\n"
 
         if [ $? -ne 0 ]; then
             logError "Error while installing Kubernetes\n"
